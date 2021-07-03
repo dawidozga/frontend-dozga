818ddd9d8b296ab1c98df439c6f2bf1035b23337
dawidozga <dawidozga93@gmail.com>
2021-07-03 19:48:38 +0200

    Add header.php and footer.php files

---
+++ footer.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * The template for displaying the footer
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package frontend-dozga
+ */
+
+?>
+
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+
+</html>
